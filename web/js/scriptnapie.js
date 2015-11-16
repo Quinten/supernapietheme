d918e6fe077776e5c1d60a0519d4004222524426
Quinten Clause <quintenclause@hotmail.com>
2015-11-16 12:17:16 +0100

    set custom menu item to active via js

--- web/js/scriptnapie.js
+++ web/js/scriptnapie.js
@@ -14,18 +14,26 @@ define([
         media: '(min-width: 768px)',
         // Switch to Desktop Version
         entry: function () {
-            console.log('desktop switch');
+            //console.log('desktop switch');
             // ...
         },
         // Switch to Mobile Version
         exit: function () {
-            console.log('mobile switch');
+            //console.log('mobile switch');
             // ...
         }
     });
 
     // document ready
-    console.log(document.readyState); // complete
+    //console.log(document.readyState); // complete
     // ...
+
+    // check if custom items in topmenu can be set to active
+    $('.navigation a.level-top').each(function (index) {
+        var href = $(this).attr("href");
+        if (location.href == href || location.pathname == href) {
+            $(this).parent().addClass('active');
+        }
+    });
 
 });
