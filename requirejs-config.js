ce1bb8d12770c9118ad05810eb9eadd413790779
Quinten Clause <quintenclause@hotmail.com>
2016-01-06 23:34:41 +0100

    Added resposiveslides

--- requirejs-config.js
+++ requirejs-config.js
@@ -6,5 +6,11 @@
 var config = {
     deps: [
         'js/scriptnapie'
-    ]
+    ],
+    paths: {
+        responsiveslides: 'js/responsiveslides.min'
+    },
+    shim: {
+        responsiveslides: ['jquery']
+    }
 };
